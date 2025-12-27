9ee015063e08f90171ad40d4aee7c9bdf4ad15d4
Wotanday <ccc26732190@gmail.com>
2025-12-28 02:23:26 +0800

    搞出了select

--- project.py
+++ project.py
@@ -1,7 +1,8 @@
 import tkinter as tk
-from tkinter import scrolledtext, filedialog
 import pandas as pd
 import Surveying
+from tkinter import scrolledtext, filedialo
+from tkinter import ttk
 
 # -------------------------
 # 主視窗
@@ -70,33 +71,18 @@ def show_content():
 # -------------------------
 # 計算並輸出結果
 # -------------------------
-def output():
-    output_box.config(state=tk.NORMAL)
-    #get value if is not in globals give a fuck
-    text1=inputbar1.get() if 'inputbar1' in globals() else ""
-    text2=inputbar2.get() if 'inputbar2' in globals() else ""
-    text3=inputbar3.get() if 'inputbar3' in globals() else ""
-    #only run when var is not ""
-    if text1:
-        output_box.insert(tk.END,text1+'\n')
-        inputbar1.delete(0,tk.END)
-    if text2:
-        output_box.insert(tk.END,text2+'\n')
-        inputbar2.delete(0,tk.END)
-    if text3:
-        output_box.insert(tk.END,text3+'\n')
-        inputbar3.delete(0,tk.END)
-    output_box.config(state=tk.DISABLED)
-
 # def output():
-#     if not BS or not FS:
-#         output_box.insert(tk.END, "\n尚未讀取資料\n")
-#         return
+#     print(select_allowable_misclosure.get())
 
-#     result = Surveying.level_high(BS, FS)
+def output():
+    if not BS or not FS:
+        output_box.insert(tk.END, "\n尚未讀取資料\n")
+        return
 
-#     output_box.insert(tk.END, "\n計算結果：\n")
-#     output_box.insert(tk.END, str(result) + "\n")
+    result = Surveying.level_high(BS, FS)
+
+    output_box.insert(tk.END, "\n計算結果：\n")
+    output_box.insert(tk.END, str(result) + "\n")
 
 # -------------------------
 # 按鈕
@@ -112,27 +98,15 @@ tk.Button(window, text="calculate", command=output)\
 # bar1x=200
 bar1input=tk.StringVar()
 bar1x=0
-inputbarLabel=tk.Label(window, text="塞拎娘:")
-inputbarLabel.grid(row=0,column=1,padx=bar1x,pady=10,sticky="w")
-inputbar1=tk.Entry(window,width=5,textvariable=bar1input)
-inputbar1.grid(row=0,column=1 ,padx=bar1x+50,pady=10,sticky="w")
-
-# bar2x=0
-# inputbarLabel=tk.Label(window, text="塞拎娘:")
-# inputbarLabel.grid(row=0,column=2,padx=bar2x,pady=10,sticky="w")
-# inputbar2=tk.Entry(window,width=5)
-# inputbar2.grid(row=0,column=2 ,padx=bar2x+50,pady=10,sticky="w")
-
-# bar3x=0
-# inputbarLabel=tk.Label(window, text="塞拎娘:")
-# inputbarLabel.grid(row=0,column=3,padx=bar3x,pady=10,sticky="w")
-# inputbar3=tk.Entry(window,width=5)
-# inputbar3.grid(row=0,column=3 ,padx=bar3x+50,pady=10,sticky="w")
+inputbarLabel=tk.Label(window, text="閉合差:")
+inputbarLabel.grid(row=0,column=3,padx=bar1x,pady=10,sticky="w")
+select_allowable_misclosure=ttk.Combobox(window,width=5,values=[2,7,10,20])
+select_allowable_misclosure.grid(row=0,column=3 ,padx=bar1x+50,pady=10,sticky="w")
 # -------------------------
 # 輸出框
 # -------------------------
 output_box = scrolledtext.ScrolledText(window, width=80, height=25)
-output_box.grid(row=1, column=0, columnspan=6, padx=50, pady=20, sticky="nsew")
+output_box.grid(row=1, column=0, columnspan=6, padx=20, pady=20, sticky="nsew")
 output_box.config(state=tk.DISABLED)
 
 window.grid_rowconfigure(1, weight=1)
